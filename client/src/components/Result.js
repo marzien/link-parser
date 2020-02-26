362a09b9d28ff5f40d89a0a2b6a4e189dd71d44d
Marius Zienius <marius.zienius@gmail.com>
2020-02-26 15:22:51 +0100

    object decompoziting

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -29,22 +29,43 @@ class Result extends Component {
       })
   }
 
+  componentDidUpdate(nextProps, prevState) {
+    console.log("componentDidUpdate")
+    console.log(this.state.results)
+  }
+
+  // https://stackoverflow.com/questions/56453521/what-react-lifecycle-component-to-use-to-update-state-after-an-axios-post
+
   render() {
+    const {
+      version,
+      title,
+      headingNumber,
+      headingLevel,
+      pictureNumber,
+      largestPicture,
+      linksIntCount,
+      linksExtCount,
+      inaccesibleLink,
+      loadingTime,
+      httpStatus
+    } = this.state.results
+
     return (
       <div>
         <h2>Results</h2>
         <ul>
-          <li key={"version"}>{this.state.results.version}</li>
-          <li key={"title"}>{this.state.results.title}</li>
-          <li key={"heading number"}>{this.state.results.headingNumber}</li>
-          <li key={"headingLevel"}>{this.state.results.headingLevel}</li>
-          <li key={"picture number"}>{this.state.results.pictureNumber}</li>
-          <li key={"largest picture"}>{this.state.results.largestPicture}</li>
-          <li key={"internal links"}>{this.state.results.linksIntCount}</li>
-          <li key={"external links"}>{this.state.results.linksExtCount}</li>
-          <li key={"inaccesible link"}>{this.state.results.inaccesibleLink}</li>
-          <li key={"loading time"}>{this.state.results.loadingTime}</li>
-          <li key={"http status"}>{this.state.results.httpStatus}</li>
+          <li key={"version"}>{version}</li>
+          <li key={"title"}>{title}</li>
+          <li key={"heading number"}>{headingNumber}</li>
+          <li key={"headingLevel"}>{headingLevel}</li>
+          <li key={"picture number"}>{pictureNumber}</li>
+          <li key={"largest picture"}>{largestPicture}</li>
+          <li key={"internal links"}>{linksIntCount}</li>
+          <li key={"external links"}>{linksExtCount}</li>
+          <li key={"inaccesible link"}>{inaccesibleLink}</li>
+          <li key={"loading time"}>{loadingTime}</li>
+          <li key={"http status"}>{httpStatus}</li>
         </ul>
       </div>
     )
