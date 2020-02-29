637192861e4d0f2ef18ce04e8318bf4e8352a551
Marius Zienius <marius.zienius@gmail.com>
2020-02-29 23:55:51 +0100

    changing server port to 5000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5000
 const ENV = process.env.NODE_ENV || "Development"
 
 app.use(express.json({ extended: false }))
